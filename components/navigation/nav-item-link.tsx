e8aeb4c532c64f332d454994d6246ca62aac70e2
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-12-09 15:19:35 -0500

    Got nav working.

--- components/navigation/nav-item-link.tsx
+++ components/navigation/nav-item-link.tsx
@@ -1,15 +1,16 @@
 import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 // import Link from 'components/link';
-// import Link from 'next/link';
+import Link from 'next/link';
 
 import type { NavItemLinkProps } from './index.d';
 
-export default function NavItemLink( { name, icon }: NavItemLinkProps ) {
-	// const ListLink = <Link href={ href } />;
+export default function NavItemLink( { name, icon, href }: NavItemLinkProps ) {
 	return (
-		<ListItem button>
-			<ListItemIcon>{ icon }</ListItemIcon>
-			<ListItemText primary={ name } />
-		</ListItem>
+		<Link href={ href }>
+			<ListItem button>
+				<ListItemIcon>{ icon }</ListItemIcon>
+				<ListItemText primary={ name } />
+			</ListItem>
+		</Link>
 	);
 }
---
+++ pages/appeals/index.tsx
@@ -0,0 +1,25 @@
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Paper } from '@material-ui/core';
+
+export default function Claims() {
+	return (
+		<Container maxWidth="lg">
+			<Box my={ 4 }>
+				<Typography variant="h4" component="h1" gutterBottom>
+					Claims
+				</Typography>
+			</Box>
+			<Paper>Paper?</Paper>
+		</Container>
+	);
+}
+
+export async function getStaticProps() {
+	return {
+		props: {
+			title: 'Claims',
+		},
+	};
+}
