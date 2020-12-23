30b4ab952ca96d2fbf5989c58ae71ff7f2c07d06
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-12-22 21:50:59 -0500

    Adds tooltips to event icons.

--- components/history-table/event-icon.tsx
+++ components/history-table/event-icon.tsx
@@ -5,19 +5,40 @@ import NoteIcon from '@material-ui/icons/Comment';
 import InfoIcon from '@material-ui/icons/Info';
 
 import type { EventType } from 'global-types';
+import { Tooltip } from '@material-ui/core';
 
 const EventIcon: React.FC<{ type: EventType }> = ( { type } ) => {
 	switch ( type.toLowerCase() ) {
 		case 'payment':
-			return <PaymentIcon />;
+			return (
+				<Tooltip title="Payment">
+					<PaymentIcon />
+				</Tooltip>
+			);
 		case 'call':
-			return <CallIcon />;
+			return (
+				<Tooltip title="Call">
+					<CallIcon />
+				</Tooltip>
+			);
 		case 'appeal':
-			return <AppealIcon />;
+			return (
+				<Tooltip title="Appeal">
+					<AppealIcon />
+				</Tooltip>
+			);
 		case 'note':
-			return <NoteIcon />;
+			return (
+				<Tooltip title="Note">
+					<NoteIcon />
+				</Tooltip>
+			);
 		default:
-			return <InfoIcon />;
+			return (
+				<Tooltip title="Other">
+					<InfoIcon />
+				</Tooltip>
+			);
 	}
 };
 
