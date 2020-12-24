7775ac476b8c93a45f22edabe381fa058bcd7d2c
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-12-23 21:32:28 -0500

    Adds stub for new claim page.

---
+++ pages/claims/new.tsx
@@ -0,0 +1,25 @@
+import { Box, Container } from '@material-ui/core';
+import type { PageProps } from 'global-types';
+
+type ClaimNewPageProps = PageProps;
+
+const ClaimNewPage: React.FC<ClaimNewPageProps> = () => {
+	// TODO: Find way to inherit claim page.
+	return (
+		<Container maxWidth="md">
+			<Box my={ 4 }>
+				Hello
+			</Box>
+		</Container>
+	);
+};
+
+export async function getStaticProps(): Promise<{ props: ClaimNewPageProps }> {
+	return {
+		props: {
+			title: 'New Claim',
+		},
+	};
+}
+
+export default ClaimNewPage;
