d73bd525a226a19e981d2464e9353b9ceed45e3c
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-12-26 15:46:43 -0500

    Allows optional wrapping of footer.

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,11 @@
-import { Link, Box } from '@material-ui/core';
+import {
+	Box,
+	Container,
+	Link,
+} from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
-const Footer: React.FC = () => (
+const FooterBase: React.FC = () => (
 	<Box my={ 4 } display="flex" justifyContent="space-between" alignItems="center">
 		<Link href="https://github.com/ChaosExAnima/health-portal/blob/main/LICENSE" target="_blank" rel="noreferrer" color="inherit">
 			&copy; 2020, MIT Licensed
@@ -10,6 +14,12 @@ const Footer: React.FC = () => (
 			<GitHubIcon />
 		</Link>
 	</Box>
+);
+
+const Footer: React.FC<{ wrap?: boolean; }> = ( { wrap = false } ) => (
+	wrap
+		? <Container maxWidth="md"><FooterBase /></Container>
+		: <FooterBase />
 );
 
 export default Footer;
