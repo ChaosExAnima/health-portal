c580db7184ee38e05064232f34ae43d78368cb2b
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-12-28 01:09:49 -0500

    Makes header more flexible.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,55 +1,109 @@
 import {
 	Box,
+	Button,
+	ButtonBaseProps,
 	Fab,
 	Grid,
 	Tooltip,
 	Typography,
 } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import ButtonLink from 'components/button-link';
 import Link from 'next/link';
 import React from 'react';
 
-type ActionItemIcon = 'add' | React.ReactElement;
+type ActionItemIcon = 'add' | 'edit' | React.ReactElement;
 
-export type ActionItem = {
-	href: string;
+export type ActionItem = Omit<ButtonBaseProps, 'href' | 'color' | 'action'> & {
+	href?: string;
 	action: string;
 	icon: ActionItemIcon;
-	color?: 'primary' | 'secondary';
+	color?: 'default' | 'primary' | 'secondary';
 };
 
 type HeaderProps = {
 	title: string;
 	actions: ActionItem[];
+	buttonsBelow?: true;
 };
 
 const ActionIcon: React.FC<{ icon: ActionItemIcon }> = ( { icon } ) => {
 	if ( icon === 'add' ) {
 		return <AddIcon />;
+	} else if ( icon === 'edit' ) {
+		return <EditIcon />;
 	}
 	return icon;
 };
 
-const Header: React.FC<HeaderProps> = ( { actions, title } ) => (
-	<Box my={ 4 }>
-		<Grid container spacing={ 4 }>
-			<Grid item>
-				<Typography variant="h4" component="h1">
-					{ title }
-				</Typography>
+const HeaderTitle: React.FC<Pick<HeaderProps, 'title'>> = ( { title } ) => (
+	<Typography variant="h4" component="h1">
+		{ title }
+	</Typography>
+);
+
+const HeaderButton: React.FC<ActionItem> = ( { href, icon, action, color = 'primary', ...props } ) => (
+	href
+		? <ButtonLink
+			href={ href }
+			startIcon={ <ActionIcon icon={ icon } /> }
+			color={ color }
+			{ ...props }
+		>{ action }</ButtonLink>
+		: <Button
+			startIcon={ <ActionIcon icon={ icon } /> }
+			color={ color }
+			{ ...props }
+		>{ action }</Button>
+);
+
+const HeaderButtonsBelow: React.FC<Pick<HeaderProps, 'actions' | 'title'>> = ( { actions, title } ) => (
+	<>
+		<HeaderTitle title={ title } />
+		<Box mt={ 3 }>
+			<Grid container spacing={ 2 }>
+				{ actions.map( ( action ) => (
+					<Grid item key={ action.href }>
+						<HeaderButton { ...action } />
+					</Grid>
+				) ) }
 			</Grid>
-			{ actions.map( ( { href, action, color = 'primary', icon } ) => (
-				<Grid item key={ href }>
-					<Link href={ href }>
-						<Fab color={ color } aria-label={ action }>
-							<Tooltip title={ action }>
-								<ActionIcon icon={ icon } />
-							</Tooltip>
-						</Fab>
-					</Link>
-				</Grid>
-			) ) }
+		</Box>
+	</>
+);
+
+const HeaderFab: React.FC<ActionItem> = ( { icon, action, color = 'primary', ...props } ) => (
+	<Tooltip title={ action }>
+		<Fab aria-label={ action } color={ color } { ...props }>
+			<ActionIcon icon={ icon } />
+		</Fab>
+	</Tooltip>
+);
+
+const HeaderButtonsSide: React.FC<Pick<HeaderProps, 'actions' | 'title'>> = ( { actions, title } ) => (
+	<Grid container spacing={ 4 }>
+		<Grid item>
+			<HeaderTitle title={ title } />
 		</Grid>
+		{ actions.map( ( { href, ...props } ) => (
+			<Grid item key={ props.action }>
+				{
+					href
+						? <Link href={ href }><HeaderFab { ...props } /></Link>
+						: <HeaderFab { ...props } />
+				}
+			</Grid>
+		) ) }
+	</Grid>
+);
+
+const Header: React.FC<HeaderProps> = ( { buttonsBelow, ...props } ) => (
+	<Box my={ 4 }>
+		{ buttonsBelow
+			? <HeaderButtonsBelow { ...props } />
+			: <HeaderButtonsSide { ...props } />
+		}
 	</Box>
 );
 
