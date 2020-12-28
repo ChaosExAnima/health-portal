ebe852a838b4226702bad4ea4d9b800fdb999384
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-12-28 13:10:47 -0500

    Wires up appeals details.

--- components/breadcrumbs/index.tsx
+++ components/breadcrumbs/index.tsx
@@ -15,18 +15,16 @@ type BreadcrumbsProps = {
 	breadcrumbs: Breadcrumb[];
 };
 
-const Breadcrumbs: React.FC<BreadcrumbsProps> = ( { breadcrumbs } ) => {
-	return (
-		<Box my={ 2 }>
-			<BreadcrumbsUI aria-label="breadcrumb">
-				{ breadcrumbs.map( ( breadcrumb: Breadcrumb ) => (
-					typeof breadcrumb !== 'string'
-						? <Link color="inherit" href={ breadcrumb.href } key={ breadcrumb.href }>{ breadcrumb.name }</Link>
-						: <Typography color="textPrimary" aria-current="page" key={ breadcrumb }>{ breadcrumb }</Typography>
-				) ) }
-			</BreadcrumbsUI>
-		</Box>
-	);
-};
+const Breadcrumbs: React.FC<BreadcrumbsProps> = ( { breadcrumbs } ) => (
+	<Box my={ 2 }>
+		<BreadcrumbsUI aria-label="breadcrumb">
+			{ breadcrumbs.map( ( breadcrumb: Breadcrumb ) => (
+				typeof breadcrumb !== 'string'
+					? <Link color="inherit" href={ breadcrumb.href } key={ breadcrumb.href }>{ breadcrumb.name }</Link>
+					: <Typography color="textPrimary" aria-current="page" key={ breadcrumb }>{ breadcrumb }</Typography>
+			) ) }
+		</BreadcrumbsUI>
+	</Box>
+);
 
 export default Breadcrumbs;
--- pages/appeals/[appeal]/index.tsx
+++ pages/appeals/[appeal]/index.tsx
@@ -1,17 +1,50 @@
 import { Box, Container } from '@material-ui/core';
 
+import Header from 'components/header';
+import Breadcrumbs from 'components/breadcrumbs';
+import DetailsBox from 'components/details-box';
+import ProviderLink from 'components/provider-link';
 import { initializeApollo } from 'lib/apollo';
-import { AppealsSlugsDocument, AppealsSlugsQuery } from 'lib/apollo/queries/appeals.graphql';
+import {
+	AppealDocument,
+	AppealQuery,
+	AppealsSlugsDocument,
+	AppealsSlugsQuery,
+} from 'lib/apollo/queries/appeals.graphql';
 import { staticPathsNoData } from 'lib/static-helpers';
+import { capitalize } from 'lib/strings';
 
-import type { GetStaticPaths, GetStaticProps } from 'next';
 import type { PageProps } from 'global-types';
+import type { GetStaticPaths, GetStaticProps } from 'next';
+import HistoryTable from 'components/history-table';
 
-const AppealPage: React.FC<PageProps> = () => {
+type AppealPageProps = PageProps & AppealQuery & {
+	slug: string;
+};
+
+const AppealPage: React.FC<AppealPageProps> = ( { title, appeal } ) => {
+	if ( ! appeal ) {
+		return null;
+	}
+	const { slug, id, name, status, date, provider } = appeal;
 	return (
 		<Container maxWidth="md">
+			<Breadcrumbs breadcrumbs={ [
+				{ href: '/appeals', name: 'Appeals' },
+				name,
+			] } />
+			<Header title={ title } actions={ [
+				{ action: 'Update', icon: 'add' },
+				{ action: 'Edit appeal', href: `/appeals/${ slug }/edit`, icon: 'edit', color: 'secondary' },
+			] } />
+			<DetailsBox details={ [
+				{ name: 'Status', detail: capitalize( status ) },
+				{ name: 'For provider', detail: <ProviderLink provider={ provider } color="inherit" /> },
+				{ name: 'Last updated', detail: date },
+				{ name: 'Update due', detail: date },
+			] } />
 			<Box my={ 4 }>
-				Hello
+				<HistoryTable type="appeal" id={ id } />
 			</Box>
 		</Container>
 	);
@@ -26,10 +59,14 @@ export const getStaticPaths: GetStaticPaths = async () => {
 	};
 };
 
-export const getStaticProps: GetStaticProps<PageProps> = async () => {
+export const getStaticProps: GetStaticProps<AppealPageProps> = async () => {
+	const client = initializeApollo();
+	const { data } = await client.query<AppealQuery>( { query: AppealDocument, variables: { slug: 'foo12334' } } );
 	return {
 		props: {
 			title: 'Appeal on 1/1/2020',
+			slug: data.appeal.slug,
+			appeal: data.appeal,
 		},
 	};
 };
