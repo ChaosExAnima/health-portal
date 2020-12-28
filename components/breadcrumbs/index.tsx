57b5175722ca1f1b23e9bc421354626b3327d5ec
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-12-28 01:09:41 -0500

    Adds Breadcrumbs and Details components.

---
+++ components/breadcrumbs/index.tsx
@@ -0,0 +1,32 @@
+import {
+	Box,
+	Breadcrumbs as BreadcrumbsUI,
+	Typography,
+} from '@material-ui/core';
+
+import Link from 'components/link';
+
+type Breadcrumb = string | {
+	name: string;
+	href: string;
+};
+
+type BreadcrumbsProps = {
+	breadcrumbs: Breadcrumb[];
+};
+
+const Breadcrumbs: React.FC<BreadcrumbsProps> = ( { breadcrumbs } ) => {
+	return (
+		<Box my={ 2 }>
+			<BreadcrumbsUI aria-label="breadcrumb">
+				{ breadcrumbs.map( ( breadcrumb: Breadcrumb ) => (
+					typeof breadcrumb !== 'string'
+						? <Link color="inherit" href={ breadcrumb.href } key={ breadcrumb.href }>{ breadcrumb.name }</Link>
+						: <Typography color="textPrimary" aria-current="page" key={ breadcrumb }>{ breadcrumb }</Typography>
+				) ) }
+			</BreadcrumbsUI>
+		</Box>
+	);
+};
+
+export default Breadcrumbs;
---
+++ components/details-box/index.tsx
@@ -0,0 +1,54 @@
+import {
+	Box,
+	Paper,
+	Grid,
+	makeStyles,
+	Theme,
+	createStyles,
+	Typography,
+} from '@material-ui/core';
+import React from 'react';
+
+type Detail = {
+	name: React.ReactNode;
+	detail: React.ReactNode;
+}
+
+type DetailsBoxProps = {
+	details: Detail[];
+};
+
+const useStyles = makeStyles( ( theme: Theme ) => createStyles( {
+	detailsContainer: {
+		padding: theme.spacing( 2 ),
+	},
+	detailsName: {
+		minWidth: 200,
+	},
+} ) );
+
+const DetailsBox: React.FC<DetailsBoxProps> = ( { details } ) => {
+	const classes = useStyles();
+	return (
+		<Box my={ 4 }>
+			<Grid component={ Paper } container className={ classes.detailsContainer } spacing={ 2 }>
+				{ details.map( ( { name, detail: status }, index ) => (
+					<Grid container item key={ index }>
+						<Grid item className={ classes.detailsName }>
+							<Typography variant="body1">
+								{ name }
+							</Typography>
+						</Grid>
+						<Grid item>
+							<Typography variant="body1">
+								{ status }
+							</Typography>
+						</Grid>
+					</Grid>
+				) ) }
+			</Grid>
+		</Box>
+	);
+};
+
+export default DetailsBox;
