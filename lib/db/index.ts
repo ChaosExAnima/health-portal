0a792d0c5040fe253b346f99c82f26af0a6b8bd5
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2021-01-13 18:57:16 -0500

    Adds cache to ORM.

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,12 +2,17 @@ import { MikroORM } from '@mikro-orm/core';
 
 import config from '../../config/db';
 
+let cache: MikroORM | null = null;
+
 async function init(): Promise<MikroORM> {
 	if ( typeof window !== 'undefined' ) {
 		throw new Error( 'Loading SQL in client context' );
 	}
 
-	const orm = await MikroORM.init( config );
+	const orm = cache || await MikroORM.init( config );
+	if ( ! cache ) {
+		cache = orm;
+	}
 
 	return orm;
 }
