6c62cd94e4f794ec881ec112723ff273ebded963
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2021-02-16 00:15:38 -0500

    Updates UI for saving providers.

--- pages/providers/[provider]/edit.tsx
+++ pages/providers/[provider]/edit.tsx
@@ -1,24 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import {
-	Box,
-	Container,
-	LinearProgress,
-} from '@material-ui/core';
+import { Box, Container, LinearProgress } from '@material-ui/core';
+import { useRouter } from 'next/router';
 
 import Breadcrumbs from 'components/breadcrumbs';
 import Header from 'components/header';
 import DetailsBox, { Detail } from 'components/details-box';
-import { useProviderQuery } from 'lib/apollo/queries/providers.graphql';
-import { getStaticPaths as getRootStaticPaths, getStaticProps as getRootStaticProps } from './index';
+import {
+	useProviderEditMutation,
+	useProviderQuery,
+} from 'lib/apollo/queries/providers.graphql';
+import {
+	getStaticPaths as getRootStaticPaths,
+	getStaticProps as getRootStaticProps,
+} from './index';
 import { staticPathsEdit, staticPropsEdit } from 'lib/static-helpers';
 
 import type { GetStaticPaths, GetStaticProps } from 'next';
 import type { SinglePageProps } from 'global-types';
 import type { Provider } from 'lib/apollo/schema/index.graphqls';
 
-const ProviderEditPage: React.FC<SinglePageProps> = ( { slug } ) => {
+const ProviderEditPage: React.FC< SinglePageProps > = ( { slug } ) => {
+	const router = useRouter();
 	const [ provider, updateProvider ] = useState< Provider >();
 	const { data, loading } = useProviderQuery( { variables: { slug } } );
+	const [ saveProvider, { loading: saving } ] = useProviderEditMutation( {
+		variables: {
+			provider: {
+				name: provider?.name || '',
+				slug,
+				website: provider?.website,
+				email: provider?.email,
+				phone: provider?.phone,
+				address: provider?.address,
+			},
+		},
+	} );
 	useEffect( () => {
 		if ( data && data.provider ) {
 			updateProvider( data.provider );
@@ -38,20 +54,54 @@ const ProviderEditPage: React.FC<SinglePageProps> = ( { slug } ) => {
 	}
 	return (
 		<Container maxWidth="md">
-			<Breadcrumbs breadcrumbs={ [
-				{ href: '/providers', name: 'Providers' },
-				{ href: `/providers/${ slug }`, name: provider.name },
-				'Edit',
-			] } />
-			<Header title={ provider.name } buttonsBelow edit onChange={ ( name ) => updateProvider( { ...provider, name } ) } actions={ [
-				{ action: 'Save', icon: 'save' },
-				{ action: 'Cancel', href: `/providers/${ slug }`, icon: 'cancel', color: 'secondary' },
-			] } />
-			<DetailsBox edit onChange={ ( key, value ) => updateProvider( { ...provider, [ key ]: value } ) }>
-				<Detail name="Website" type="url" id="website">{ provider.website }</Detail>
-				<Detail name="Email" id="email">{ provider.email }</Detail>
-				<Detail name="Phone Number" id="phone">{ provider.phone }</Detail>
-				<Detail name="Address" id="address">{ provider.address }</Detail>
+			<Breadcrumbs
+				breadcrumbs={ [
+					{ href: '/providers', name: 'Providers' },
+					{ href: `/providers/${ slug }`, name: provider.name },
+					'Edit',
+				] }
+			/>
+			<Header
+				title={ provider.name }
+				buttonsBelow
+				edit
+				onChange={ ( name ) => updateProvider( { ...provider, name } ) }
+				actions={ [
+					{
+						action: 'Save',
+						icon: 'save',
+						onClick: async () => {
+							await saveProvider();
+							router.push( `/providers/${ slug }` );
+						},
+						disabled: saving,
+					},
+					{
+						action: 'Cancel',
+						href: `/providers/${ slug }`,
+						icon: 'cancel',
+						color: 'secondary',
+					},
+				] }
+			/>
+			<DetailsBox
+				edit
+				onChange={ ( key, value ) =>
+					updateProvider( { ...provider, [ key ]: value } )
+				}
+			>
+				<Detail name="Website" type="url" id="website">
+					{ provider.website }
+				</Detail>
+				<Detail name="Email" id="email">
+					{ provider.email }
+				</Detail>
+				<Detail name="Phone Number" id="phone">
+					{ provider.phone }
+				</Detail>
+				<Detail name="Address" id="address">
+					{ provider.address }
+				</Detail>
 			</DetailsBox>
 		</Container>
 	);
