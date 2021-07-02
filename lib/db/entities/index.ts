27a8d3aaf758bcad6e4edfb1463e1d8d8288f8c9
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2021-07-01 21:52:34 -0400

    Rips everything outttttt

--- lib/db/entities/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Appeal } from './appeal';
-export { default as Call } from './call';
-export { default as Claim } from './claim';
-export { default as File } from './file';
-export { default as Note } from './note';
-export { default as Payment } from './payment';
-export { default as Provider } from './provider';
-export { default as Representative } from './representative';
-export { default as Import } from './import';
