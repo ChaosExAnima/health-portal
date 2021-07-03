66c05a537515f691b925829eed82f2ac1ed63957
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2021-07-03 00:04:31 -0400

    Drops import id column for meta.

---
+++ lib/db/migrations/20210703035443_importid-to-meta.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex';
+import { TABLE_CONTENT } from '../constants';
+
+export async function up( knex: Knex ): Promise< void > {
+	await knex.schema.table( TABLE_CONTENT, ( table ) => {
+		table.dropColumn( 'importId' );
+	} );
+}
+
+export async function down( knex: Knex ): Promise< void > {
+	await knex.schema.table( TABLE_CONTENT, ( table ) => {
+		table.integer( 'importId', 10 ).unsigned().nullable().index();
+	} );
+}
