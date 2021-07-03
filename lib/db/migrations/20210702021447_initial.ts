f2008f3d3324626b92e3b4eec55a1d76765d2473
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2021-07-02 23:37:27 -0400

    Removes foreign keys, cuz more trouble than it's worth.

--- lib/db/migrations/20210702021447_initial.ts
+++ lib/db/migrations/20210702021447_initial.ts
@@ -8,28 +8,14 @@ import {
 	TABLE_PROVIDERS,
 	TABLE_RELATIONS,
 } from '../constants';
-import { ContentDB, ImportDB, MetaDB, ProviderDB, RelationDB } from '../types';
 
 export async function up( knex: Knex ): Promise< void > {
-	const addCreated = ( table: Knex.TableBuilder ): void => {
+	const addCreated = ( table: Knex.TableBuilder ): Knex.ColumnBuilder =>
 		table.dateTime( 'created' ).defaultTo( knex.fn.now() );
-	};
 	const addRef = (
 		table: Knex.TableBuilder,
-		name: string,
-		refTable?: string,
-		refColumn = 'id'
-	): Knex.ColumnBuilder => {
-		let column = table.integer( name, 10 ).unsigned();
-		if ( refTable && refColumn ) {
-			column = column
-				.references( refColumn )
-				.inTable( refTable )
-				.onDelete( 'CASCADE' )
-				.onUpdate( 'CASCADE' );
-		}
-		return column;
-	};
+		name: string
+	): Knex.ColumnBuilder => table.integer( name, 10 ).unsigned();
 
 	await knex.schema.createTable( TABLE_IMPORTS, ( table ) => {
 		table.increments();
@@ -49,7 +35,7 @@ export async function up( knex: Knex ): Promise< void > {
 		table.text( 'address' );
 		table.string( 'website' );
 		table.string( 'email' );
-		addRef( table, 'importId', TABLE_IMPORTS ).nullable().index();
+		addRef( table, 'importId' ).nullable().index();
 	} );
 
 	await knex.schema.createTable( TABLE_CONTENT, ( table ) => {
@@ -59,26 +45,18 @@ export async function up( knex: Knex ): Promise< void > {
 		table.enum( 'type', CONTENT_ALL ).notNullable();
 		table.text( 'info' ).nullable();
 		table.string( 'status' ).nullable().index();
-		addRef( table, 'providerId', TABLE_PROVIDERS ).nullable().index();
-		addRef( table, 'importId', TABLE_IMPORTS ).nullable().index();
+		addRef( table, 'providerId' ).nullable().index();
+		addRef( table, 'importId' ).nullable().index();
 
 		table
 			.index( [ 'type', 'identifier' ] )
 			.comment( 'Should use to query types.' );
 	} );
 
-	await knex.schema.table( TABLE_IMPORTS, ( table ) => {
-		table
-			.foreign( 'fileId' )
-			.references( 'id' )
-			.inTable( TABLE_CONTENT )
-			.onDelete( 'SET NULL' );
-	} );
-
 	await knex.schema.createTable( TABLE_META, ( table ) => {
 		table.increments();
 		addCreated( table );
-		addRef( table, 'contentId', TABLE_CONTENT ).notNullable().index();
+		addRef( table, 'contentId' ).notNullable().index();
 		table.string( 'key', 100 ).notNullable();
 		table.string( 'value' );
 		table.json( 'meta' );
@@ -89,8 +67,8 @@ export async function up( knex: Knex ): Promise< void > {
 	await knex.schema.createTable( TABLE_RELATIONS, ( table ) => {
 		table.increments();
 		addCreated( table );
-		addRef( table, 'from', TABLE_CONTENT ).notNullable();
-		addRef( table, 'to', TABLE_CONTENT ).notNullable();
+		addRef( table, 'from' ).notNullable();
+		addRef( table, 'to' ).notNullable();
 		table.json( 'meta' );
 
 		table
@@ -99,27 +77,7 @@ export async function up( knex: Knex ): Promise< void > {
 	} );
 }
 
-async function dropForeign< Table extends Record< string, unknown > >(
-	knex: Knex,
-	tableName: string,
-	keys: keyof Table | ( keyof Table )[]
-): Promise< void > {
-	if ( await knex.schema.hasTable( tableName ) ) {
-		await knex.schema.table( tableName, ( table ) => {
-			table.dropForeign( keys as string | string[] );
-		} );
-	}
-}
-
 export async function down( knex: Knex ): Promise< void > {
-	await dropForeign< ImportDB >( knex, TABLE_IMPORTS, 'fileId' );
-	await dropForeign< ProviderDB >( knex, TABLE_PROVIDERS, 'importId' );
-	await dropForeign< ContentDB >( knex, TABLE_CONTENT, [
-		'providerId',
-		'importId',
-	] );
-	await dropForeign< MetaDB >( knex, TABLE_META, 'contentId' );
-	await dropForeign< RelationDB >( knex, TABLE_RELATIONS, [ 'to', 'from' ] );
 	await knex.schema.dropTableIfExists( TABLE_CONTENT );
 	await knex.schema.dropTableIfExists( TABLE_IMPORTS );
 	await knex.schema.dropTableIfExists( TABLE_META );
