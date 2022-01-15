8022eddbee4700c93f60e022828654e02cee3e5b
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-01-14 21:14:49 -0500

    Updates global types

--- global-types.d.ts
+++ global-types.d.ts
@@ -1,33 +1,34 @@
 import { GetStaticProps } from 'next';
 import { Entity } from 'lib/entities/types';
 
-export type Optional< T, K extends keyof T > = Omit< T, K > & Partial< T >;
-export type Nullable< T > = T | null;
-export type StringKeys< T > = Extract< keyof T, string >;
+type Optional< T, K extends keyof T > = Omit< T, K > & Partial< T >;
+type Nullable< T > = T | null;
+type StringKeys< T > = Extract< keyof T, string >;
+type MaybeArray< T > = T | T[];
 
-export type PaginatedPageContext = {
+type PaginatedPageContext = {
 	page: string;
 }
-export type SinglePageContext = {
+type SinglePageContext = {
 	slug: string;
 };
 
-export type PageProps = {
+type PageProps = {
 	title: string;
 };
 
-export type PaginatedPageProps< T extends Entity > = PageProps & {
+type PaginatedPageProps< T extends Entity > = PageProps & {
 	currentPage: number;
 	totalPages: number;
 	records: T[];
 };
 
-export type SinglePageProps< T extends Entity > = PageProps & {
+type SinglePageProps< T extends Entity > = PageProps & {
 	id: number;
 	slug: string;
 	record: T;
 };
 
-export type GetSinglePageProps< T extends Entity > = GetStaticProps< SinglePageProps< T >, SinglePageContext >;
+type GetSinglePageProps< T extends Entity > = GetStaticProps< SinglePageProps< T >, SinglePageContext >;
 
-export type onChangeFunc = ( value: string ) => void;
+type onChangeFunc = ( value: string ) => void;
