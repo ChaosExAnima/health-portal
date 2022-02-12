3ed277394a17506efe0469f37a80a4a9606883a4
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-02-12 14:59:30 -0500

    Adds Emotion to terms field.

--- components/term-field/chips.tsx
+++ components/term-field/chips.tsx
@@ -1,21 +1,6 @@
 import { Chip } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import { without } from 'lodash';
 import { TermFieldChipsProps } from './types';
-
-const PREFIX = 'chips';
-
-const classes = {
-	chip: `${ PREFIX }-chip`,
-};
-
-// TODO jss-to-styled codemod: The Fragment root was replaced by div. Change the tag if needed.
-const Root = styled( 'div' )( () => ( {
-	[ `& .${ classes.chip }` ]: {
-		margin: 3,
-		maxWidth: 'calc( 100% - 6px)',
-	},
-} ) );
 
 export default function TermFieldChips( {
 	terms,
@@ -30,16 +15,18 @@ export default function TermFieldChips( {
 	};
 
 	return (
-		<Root>
+		<>
 			{ terms.map( ( term ) => (
 				<Chip
 					key={ term }
 					{ ...ChipProps }
-					className={ classes.chip }
+					sx={ ( theme ) => ( {
+						maxWidth: `calc( 100% - ${ theme.spacing( 3 ) } )`,
+					} ) }
 					label={ term }
 					onDelete={ onDelete( term ) }
 				/>
 			) ) }
-		</Root>
+		</>
 	);
 }
