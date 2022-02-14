48811ff10a13517ed01e247215a460f8b1d9f171
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-02-13 23:31:30 -0500

    Attempts fix to misaligned coverage reports.

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,12 @@ const customJestConfig = {
 	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
 	moduleDirectories: [ 'node_modules', '<rootDir>/' ],
 	testEnvironment: 'jest-environment-jsdom',
+	transform: {
+		'^.+\\.(js|jsx|ts|tsx)$': [
+			'babel-jest',
+			{ presets: [ 'next/babel' ] },
+		],
+	},
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
