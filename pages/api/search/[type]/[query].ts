def00ca4ab45e2cb08cb499472d02adbd666cd3b
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-05-15 21:28:21 -0400

    Wires up autocomplete.

--- pages/api/search/[type]/[query].ts
+++ pages/api/search/[type]/[query].ts
@@ -1,9 +1,23 @@
+import { Knex } from 'knex';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import * as constants from 'lib/constants';
+import {
+	getIdColumn,
+	queryAllProviders,
+	queryContentType,
+	queryRelatedProviders,
+} from 'lib/db/helpers';
 import { inReadonlyArray } from 'lib/entities/utils';
 
-import type { AutocompleteResponse } from 'components/form/types';
+import type {
+	AutocompleteOption,
+	AutocompleteResponse,
+} from 'components/form/types';
+import type { ContentDB, ProviderDB } from 'lib/db/types';
+
+type AutocompleteDB = ContentDB | ProviderDB;
+type ProviderMap = Record< number, ProviderDB[ 'name' ] >;
 
 export default async function handler(
 	req: NextApiRequest,
@@ -18,7 +32,6 @@ export default async function handler(
 			errors: [ 'Invalid query or type' ],
 		} );
 	}
-	// @TODO Finish this!
 
 	if ( ! inReadonlyArray( type, constants.API_ENTITIES ) ) {
 		return res.json( {
@@ -26,9 +39,69 @@ export default async function handler(
 			errors: [ 'Invalid type' ],
 		} );
 	}
+	const entityType: constants.API_ENTITY_TYPE = type as constants.API_ENTITY_TYPE;
+
+	let dbQuery: Knex.QueryBuilder;
+	if ( entityType === 'provider' ) {
+		dbQuery = queryAllProviders().where( 'name', 'like', `%${ query }%` );
+	} else {
+		dbQuery = queryContentType( entityType ).where(
+			'identifier',
+			'like',
+			`%${ query }%`
+		);
+	}
+	const results: AutocompleteDB[] = await dbQuery.limit( 10 );
+	const providers = await getCallProviders( results );
 
 	res.json( {
 		success: true,
-		options: [ { label: 'Test', id: 123 } ],
+		options: (
+			await Promise.all(
+				results.map( ( row ) => rowToOption( row, providers ) )
+			)
+		 ).filter( ( opt ): opt is AutocompleteOption => opt !== null ),
 	} );
+}
+
+async function getCallProviders(
+	rows: AutocompleteDB[]
+): Promise< ProviderMap > {
+	const calls: ContentDB[] = rows.filter(
+		( row ): row is ContentDB =>
+			'type' in row && row.type === constants.CONTENT_CALL
+	);
+	if ( ! calls.length ) {
+		return {};
+	}
+	const providers = await queryRelatedProviders(
+		getIdColumn( calls, 'providerId' )
+	).select( 'id', 'name' );
+	return Object.fromEntries(
+		providers.map( ( { id, name } ) => [ id, name ] )
+	);
+}
+
+async function rowToOption(
+	row: AutocompleteDB,
+	providers: ProviderMap
+): Promise< AutocompleteOption | null > {
+	let label: string;
+
+	if ( 'name' in row ) {
+		label = row.name;
+	} else if ( row.type === constants.CONTENT_CALL ) {
+		if ( row.providerId && Object.hasOwn( providers, row.providerId ) ) {
+			label = `Call with ${ providers[ row.providerId ] }`;
+		} else {
+			return null;
+		}
+	} else {
+		label = row.identifier;
+	}
+
+	return {
+		id: row.id,
+		label,
+	};
 }
